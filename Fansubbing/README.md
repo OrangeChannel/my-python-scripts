cec9283b2e265b7bdf3cd462bc51a29f08d1d680
Dave <orangechannel@pm.me>
2020-02-06 23:09:56 -0500

    Fix major "cleaner" overwriting issue and mention limitations

--- Fansubbing/README.md
+++ Fansubbing/README.md
@@ -12,43 +12,47 @@ python fansub_utils.py --renamer
 
 # Current Functions:
 
-## hasher (`-H`)
+## hasher `-H`
 Appends a CRC32 hash to the filenames in the folder.
 
 Before: `[ChannelOrange] Watashi ni ga Maiorita! - 05 (BD 720p).mkv`
 
 After: `[ChannelOrange] Watashi ni ga Maiorita! - 05 (BD 720p) [ABCD1234].mkv`
 
-## checker (`-K`)
+## checker `-K`
 Verifies CRC32 hases in filenames.
 
-## remover (`-X`)
+## remover `-X`
 Removes CRC32 hashes from the end of filenames (can handle whitespace before the hash).
 
 Before:  `[ChannelOrange] Watashi ni ga Maiorita! - 05 (BD 720p) [ABCD1234].mkv`
 
 After:  `[ChannelOrange] Watashi ni ga Maiorita! - 05 (BD 720p).mkv`
 
-## renamer (`-R`)
+## renamer `-R`
 Batch renames files `ep01.mkv`, `ep02.mkv`, ...`ep13.mkv` to `[Group] Title - ## (SRC RESp).mkv`
 - i.e. `ep05.mkv` --> `[ChannelOrange] Watashi ni ga Maiorita! - 05 (BD 720p).mkv`
 
-## cleaner (`-C`)
+## cleaner `-C`
 Reformats arbitrary filenames to `[Group] Title - ## (SRC RESp).mkv` based only on episode number found in original
  title.
  
 ##### Before
 
-- `re:zero 06.mkv`
-- `[Trash] rezero episode 12[ABCD1234].mkv`
-- `[Trash] Re:Zero 5.mkv`
-- `[Garbage *$ ReZERO 1.mkv`
+- `re:zero 06v3.mkv`
+- `[Trash] rezero s2 episode 12[ABCD1234].mkv`
+- `[Trash] Re:Zero2_5.mkv`
+- `[Garbage *$ s1 ReZERO 1.mkv`
 
 #### After
 
-- `[ChannelOrange] Re:Zero Season 3 - 01 (WEB 720p).mkv`
-- `[ChannelOrange] Re:Zero Season 3 - 05 (WEB 720p).mkv`
-- `[ChannelOrange] Re:Zero Season 3 - 06 (WEB 720p).mkv`
-- `[ChannelOrange] Re:Zero Season 3 - 12 (WEB 720p).mkv`
+- `[ChannelOrange] Re:Zero - 01 (WEB 720p).mkv`
+- `[ChannelOrange] Re:Zero - 05 (WEB 720p).mkv`
+- `[ChannelOrange] Re:Zero - 06 (WEB 720p).mkv`
+- `[ChannelOrange] Re:Zero - 12 (WEB 720p).mkv`
 
-*Note: only will find episode numbers with a space before them (`_##` or `_#`).*
+*Note: only will find episode numbers with a space (or underscore) before them (`_##` or `_#`).*
+
+*WARNING: files like `[ChannelOrange] Re:Zero Season 3 - 12 (WEB 720p).mkv` containing a season number will all be
+ overwritten with the same name into one file. This is **NOT** reversible so make sure the episodes all have a
+  __single unique__ one-two digit number in them.*
--- Fansubbing/fansub_utils.py
+++ Fansubbing/fansub_utils.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
+"""
+Dependencies:
+    which
+    mv
+    fd :      https://www.archlinux.org/packages/community/x86_64/fd/ OR `cargo install fd-find`
+    rhash :   https://www.archlinux.org/packages/extra/x86_64/rhash/
+    rnr :     https://aur.archlinux.org/packages/rnr/ OR `cargo install rnr`
+    xdelta3 : https://www.archlinux.org/packages/community/x86_64/xdelta3/
+    7z :      https://www.archlinux.org/packages/extra/x86_64/p7zip/
+"""
 __author__ = 'Dave <orangechannel@pm.me>'
 __date__ = '6 February 2020'
-from typing import List
+import argparse
+import re
 import shlex
 import subprocess
 from sys import version_info
-import argparse
-import re
+from typing import List
 
 if version_info[0] != 3 or version_info[1] < 8:
     raise SystemError("Python version 3.8+ required!")
@@ -39,7 +49,6 @@ def renamer(group_name: str, title: str, src: str, resolution: int):
     args = shlex.split(cmd)
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
     filenum = len(proc.stdout.read().decode().splitlines())
-    proc.kill()
     print('{} files have been renamed.'.format(filenum))
 
 
@@ -54,7 +63,6 @@ def hasher():
     for i in proc.stdout.read().decode().splitlines():
         if i[0] != r';': filenum += 1
 
-    proc.kill()
     print('{} files have been renamed.'.format(filenum))
 
 
@@ -65,7 +73,6 @@ def remover():
     args = shlex.split(cmd)
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
     filenum = len(proc.stdout.read().decode().splitlines())
-    proc.kill()
     print('{} files have been renamed.'.format(filenum))
 
 
@@ -79,6 +86,7 @@ def checker():
 
 
 def cleaner():
+    """Renames files based on unique one-two digit number found in original filename."""
     group_name = input('Group name: ')
     title = input('Show title: ')
     src = input('Source (TV, WEB, BD, DVD): ')
@@ -89,22 +97,25 @@ def cleaner():
     orig_names = subprocess.Popen(args, stdout=subprocess.PIPE).stdout.read().decode().splitlines()
     orig_names = [i.rstrip() for i in orig_names]
     for k, name in enumerate(orig_names):
-        m = re.search(r'\s(?P<num>\d{1,2})', name)
+        m = re.search(r'(\s|_)(?P<num>\d{1,2})\D', name)
         if m:
             cmd = '{} "{}" "[{}] {} - {:02d} ({} {}p).mkv"'.format(paths['mv'], name, group_name, title, int(m.group('num')), src, resolution)
             args = shlex.split(cmd)
             subprocess.Popen(args)
 
 
+def diff(numbers: List[int]):
+    """TODO"""
+    print(numbers)
 
 
-
-parser = argparse.ArgumentParser(description='Utility functions for fansubbing.', epilog=r'Issue tracker on GitHub: https://github.com/OrangeChannel', prog='fansub_utils.py', usage='%(prog)s [mode]')
+parser = argparse.ArgumentParser(description='Utility functions for fansubbing.', epilog=r'More information on GitHub: https://github.com/OrangeChannel/my-python-scripts/blob/master/Fansubbing/README.md', prog='fansub_utils.py', usage='%(prog)s <mode> [opts]')
 parser.add_argument('-H', '--hasher', dest='hasher', action='store_true', default=False, help='Appends a CRC32 hash to the filenames: "file.mkv" -> "file [ABCD1234].mkv"')
 parser.add_argument('-K', '--checker', dest='checker', action='store_true', default=False, help='Verifies CRC32 hashes in filenames')
 parser.add_argument('-X', '--remover', dest='remover', action='store_true', default=False, help='Removes a CRC32 hash from filenames: "file [ABCD1234].mkv" -> "file.mkv"')
 parser.add_argument('-R', '--renamer', dest='renamer', action='store_true', default=False, help=r'Useful for batch renaming "ep##.mkv" to "[Group] Title - ## (SRC RESp).mkv"')
-parser.add_argument('-C', '--cleaner', dest='cleaner', action='store_true', default=False, help=r'Similar to renamer but works with any original filename format containing " ##" or " #"')
+parser.add_argument('-C', '--cleaner', dest='cleaner', action='store_true', default=False, help=r'Similar to renamer but works with any original filename format containing "_##"/" ##" or "_#"/" #"')
+parser.add_argument('-D', '--diff', dest='diff', metavar='int', type=int, nargs='+', default=None, help='Creates an xdelta3 patch archive for one or more v2\'s specified by episode number(s)')
 
 
 def _run():
@@ -120,6 +131,8 @@ def _run():
         checker()
     elif args.cleaner:
         cleaner()
+    elif args.diff:
+        diff([*map(int, args.diff)])
 
     else:
         print('Specify a function to run. Use `-h` for help.')
